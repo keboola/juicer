85320926618cc3cdee4971e02b6b8e7823bf1bc1
Ondrej Popelka <odinuv@gmail.com>
2017-05-04 15:12:52 +0200

    chore: add phpcs and phpstan to tests

--- phpunit.sh
+++ phpunit.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
 composer selfupdate
-composer install -n
+composer install --no-interaction
 
+./vendor/bin/phpcs --standard=psr2 --ignore=vendor -n .
+# ./vendor/bin/phpstan analyse . --level=4 || true
 ./vendor/bin/phpunit "$@"
