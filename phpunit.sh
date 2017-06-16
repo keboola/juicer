324fe6469e609b788f5483bb4486a107b665e5aa
Odin <odinuv@gmail.com>
2017-06-16 13:57:57 +0200

    fix: remove unused stuff

--- phpunit.sh
+++ phpunit.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -e 
+set -e
 
 php --version
 
 echo "Starting tests" >&1
 ./vendor/bin/phpcs --standard=psr2 --ignore=vendor -n .
-./vendor/bin/phpstan analyse --level=4 Client Common Config Exception Extractor Filesystem Pagination Parser Tests || true
+./vendor/bin/phpstan analyse --level=4 Client Config Exception Extractor Filesystem Pagination Parser Tests || true
 
 ./vendor/bin/phpunit --coverage-clover build/logs/clover.xml
 ./vendor/bin/test-reporter
