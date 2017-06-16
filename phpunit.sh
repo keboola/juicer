8b84e9ab325f5d13c7ddcade1f817e1b3bbbbc6a
Odin <odinuv@gmail.com>
2017-06-16 09:28:58 +0200

    chore: proper dockerized testing

---
+++ phpunit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e 
+
+php --version
+
+echo "Starting tests" >&1
+./vendor/bin/phpcs --standard=psr2 --ignore=vendor -n .
+./vendor/bin/phpstan analyse --level=4 Client Common Config Exception Extractor Filesystem Pagination Parser Tests || true
+
+./vendor/bin/phpunit --coverage-clover build/logs/clover.xml
+./vendor/bin/test-reporter
+
+echo "Tests Finished" >&1
