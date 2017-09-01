cbca7d334d0af7c5a0b066e8ba6d94e19c4ef313
odinuv <odinuv@gmail.com>
2017-09-01 12:29:18 +0200

    tests: add tests for legacy parser and switching

--- Tests/Parser/JsonTest.php
+++ Tests/Parser/JsonTest.php
@@ -79,32 +79,32 @@ class JsonTest extends ExtractorTestCase
         );
     }
 
-    public function testUpdateStruct()
+    public function testLegacyStruct()
     {
         $json = '{
-            "root.arr.arr1": {
-                "c": "string"
+            "json_parser.struct": {
+                "root.arr.arr1": {
+                    "c": "scalar"
+                },
+                "root.arr.arr2": {
+                    "data": "scalar"
+                },
+                "root.arr": {
+                    "a": "scalar",
+                    "b": "scalar",
+                    "arr1": "arrayOfobject",
+                    "arr2": "arrayOfscalar"
+                },
+                "root": {
+                    "id": "scalar",
+                    "arr": "arrayOfobject"
+                }
             },
-            "root.arr.arr2": {
-                "data": "integer"
-            },
-            "root.arr": {
-                "a": "string",
-                "b": "double",
-                "arr1": "array",
-                "arr2": "array"
-            },
-            "root": {
-                "id": "integer",
-                "arr": "array"
-            }
+            "json_parser.structVersion": 2
         }';
-        $parser = new Json(new NullLogger(), $json, 3);
-
-        $updated = self::callMethod($parser, 'updateStruct', [$struct]);
-
-        $parser->getParser()->getAnalyzer()->getStructure()->load($updated);
-
+        $handler = new TestHandler();
+        $logger = new Logger('null', [$handler]);
+        $parser = new Json($logger, json_decode($json, true), 3);
         $parser->process([
             (object) [
                 'id' => 1,
@@ -119,40 +119,23 @@ class JsonTest extends ExtractorTestCase
             ]
         ], 'root');
 
-        self::assertEquals(
-            [
-                'root.arr.arr1' => ['c' => 'scalar'],
-                'root.arr.arr2' => ['data' => 'scalar'],
-                'root.arr' => [
-                    'a' => 'scalar',
-                    'b' => 'scalar',
-                    'arr1' => 'arrayOfobject',
-                    'arr2' => 'arrayOfscalar'
-                ],
-                'root' => [
-                    'id' => 'scalar',
-                    'arr' => 'arrayOfobject'
-                ]
-            ],
-            $parser->getParser()->getAnalyzer()->getStructure()->getData()
-        );
-
         self::assertEquals('"id","arr"
 "1","root_a52f96d95586c8de1e8fa67b77597262"
-', file_get_contents($parser->getParser()->getCsvFiles()['root']));
+', file_get_contents($parser->getResults()['root']));
 
         self::assertEquals('"a","b","arr1","arr2","JSON_parentId"
 "hello","1.1","root.arr_a75f0a3e0b848d52033929a761e6c997","root.arr_a75f0a3e0b848d52033929a761e6c997","root_a52f96d95586c8de1e8fa67b77597262"
-', file_get_contents($parser->getParser()->getCsvFiles()['root_arr']));
+', file_get_contents($parser->getResults()['root_arr']));
 
         self::assertEquals('"c","JSON_parentId"
 "d","root.arr_a75f0a3e0b848d52033929a761e6c997"
-', file_get_contents($parser->getParser()->getCsvFiles()['root_arr_arr1']));
+', file_get_contents($parser->getResults()['root_arr_arr1']));
 
         self::assertEquals('"data","JSON_parentId"
 "1","root.arr_a75f0a3e0b848d52033929a761e6c997"
 "2","root.arr_a75f0a3e0b848d52033929a761e6c997"
-', file_get_contents($parser->getParser()->getCsvFiles()['root_arr_arr2']));
+', file_get_contents($parser->getResults()['root_arr_arr2']));
+        self::assertTrue($handler->hasWarning('Using legacy JSON parser, because it is in configuration state.'));
     }
 
     public function testProcessNoData()
@@ -163,5 +146,114 @@ class JsonTest extends ExtractorTestCase
 
         $parser->process([], 'empty');
         self::assertTrue($logHandler->hasDebug("No data returned in 'empty'"));
+    }
+
+    public function testLegacyStructConflict()
+    {
+        $json = [
+            'json_parser.struct' => [
+                'root' => [
+                    'id' => 'scalar',
+                    'some_property' => 'scalar'
+                ]
+            ],
+            'json_parser.structVersion' => 2
+        ];
+        $handler = new TestHandler();
+        $logger = new Logger('null', [$handler]);
+        $parser = new Json($logger, $json, 3);
+        $parser->process([
+            (object) [
+                'id' => 1,
+                'some_property' => 'first_value',
+                'some.property' => 'second_value'
+            ]
+        ], 'root');
+
+        self::assertEquals("\"id\",\"some_property\",\"48d4950101ffec0dc0bd1c76f77ca4ef\"\n" .
+            "\"1\",\"second_value\",\"\"\n",
+            file_get_contents($parser->getResults()['root'])
+        );
+        self::assertTrue($handler->hasWarning('Using legacy JSON parser, because it is in configuration state.'));
+    }
+
+    public function testStructConflict()
+    {
+        $json = [
+            'json_parser.struct' => [
+                'root' => [
+                    'nodeType' => 'array',
+                    '[]' => [
+                        'nodeType' => 'object',
+                        '_id' => [
+                            'nodeType' => 'scalar'
+                        ],
+                        '_some_property' => [
+                            'nodeType' => 'scalar',
+                        ],
+                        '_some.property' => [
+                            'nodeType' => 'scalar'
+                        ],
+                    ]
+                ],
+            ],
+            'json_parser.structVersion' => 3
+        ];
+        $handler = new TestHandler();
+        $logger = new Logger('null', [$handler]);
+        $parser = new Json($logger, $json, 3);
+        $parser->process([
+            (object) [
+                'id' => 1,
+                'some_property' => 'first_value',
+                'some.property' => 'second_value'
+            ]
+        ], 'root');
+
+        self::assertEquals("\"id\",\"some_property\",\"some_property_u0\"\n" .
+            "\"1\",\"first_value\",\"second_value\"\n",
+            file_get_contents($parser->getResults()['root'])
+        );
+        self::assertFalse($handler->hasWarning('Using legacy JSON parser, because it is in configuration state.'));
+    }
+
+    public function testLegacyNoStructExplicitVersionConflict()
+    {
+        $handler = new TestHandler();
+        $logger = new Logger('null', [$handler]);
+        $parser = new Json($logger, [], 2);
+        $parser->process([
+            (object) [
+                'id' => 1,
+                'some_property' => 'first_value',
+                'some.property' => 'second_value'
+            ]
+        ], 'root');
+
+        self::assertEquals("\"id\",\"some_property\",\"48d4950101ffec0dc0bd1c76f77ca4ef\"\n" .
+            "\"1\",\"second_value\",\"\"\n",
+            file_get_contents($parser->getResults()['root'])
+        );
+        self::assertTrue($handler->hasWarning('Using legacy JSON parser, because it has been explicitly requested.'));
+    }
+
+    public function testNoStructExplicitVersionConflict()
+    {
+        $handler = new TestHandler();
+        $logger = new Logger('null', [$handler]);
+        $parser = new Json($logger, [], 3);
+        $parser->process([
+            (object) [
+                'id' => 1,
+                'some_property' => 'first_value',
+                'some.property' => 'second_value'
+            ]
+        ], 'root');
+
+        self::assertEquals("\"id\",\"some_property\",\"some_property_u0\"\n" .
+            "\"1\",\"first_value\",\"second_value\"\n",
+            file_get_contents($parser->getResults()['root'])
+        );
+        self::assertFalse($handler->hasWarning('Using legacy JSON parser, because it has been explicitly requested.'));
     }
 }
